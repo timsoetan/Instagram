ac64fe05e36617533287f5718c7440a12cd9ed5b
Timi Soetan <timisoetan@fb.com>
2019-07-09 11:36:33 -0700

    "Create Post and Query classes"

---
+++ app/src/main/java/com/example/instagram/Model/ParseApp.java
@@ -0,0 +1,24 @@
+package com.example.instagram.Model;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
+
+        final Parse.Configuration configuration = new Parse.Configuration.Builder(this)
+                .applicationId("fbu-ig")
+                .clientKey("penn-engineering")
+                .server("http://instagram-android-client.herokuapp.com/parse")
+                .build();
+
+        Parse.initialize(configuration);
+    }
+}
